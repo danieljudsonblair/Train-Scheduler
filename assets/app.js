eda08777f960fed16f6769ed38a974bc6f3875e3
s1191 <danieljudsonblair@gmail.com>
2019-01-08 21:34:54 -0500

    connected to firebase, static table colums complete

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,5 @@
-// Steps to complete:
 
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
 
-// 1. Initialize Firebase
 var config = {
     apiKey: "AIzaSyAlymNtfOTAgwWMItAn99BPZXpqaakzohc",
     authDomain: "train-scheduler-abf00.firebaseapp.com",
@@ -19,12 +11,12 @@ var config = {
   firebase.initializeApp(config);
   
   var database = firebase.database();
+  setInterval(function() {$("#day").text(moment().format('LLL'));}, 1000);
   
-  // 2. Button for adding Employees
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
   
-    // Grabs user input
+
     var tName = $("#train-name-input").val().trim();
     var tDes = $("#destination-input").val().trim();
     var tFt = $("#ftt-input").val().trim(); 
@@ -39,11 +31,11 @@ var config = {
       Frequency: tFq
     };
   
-    // Uploads employee data to the database
+    // Uploads train data to the database
     database.ref().push(newTrain);
   
- 
-    // alert("Train successfully added");
+  
+
   
     // Clears all of the text-boxes
     $("#train-name-input").val("");
@@ -52,48 +44,41 @@ var config = {
     $("#frequency-input").val("");
   });
   
-  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  var newRow
   database.ref().on("child_added", function(childSnap) {
     
   
     // Store everything into a variable.
     var trainName = childSnap.val().Name;
     var trainDes = childSnap.val().Destination;
-    var trainFTime = childSnap.val().FirstTrainTime;
     var trainFreq = childSnap.val().Frequency;
 
-  
-    // Prettify the employee start
-    var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
-  
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-    var empMonths = moment().diff(moment(empStart, "X"), "months");
-    console.log(empMonths);
-  
-    // Calculate the total billed rate
-    var empBilled = empMonths * empRate;
-    console.log(empBilled);
+
+  console.log(trainName);
+  console.log(trainDes);
+  // console.log(trainFTime);
+  console.log(trainFreq);
+
   
     // Create the new row
-    var newRow = $("<tr>").append(
+      newRow = $("<tr>").append(
       $("<td>").text(trainName),
       $("<td>").text(trainDes),
-    //   $("<td>").text(empStartPretty),
-      $("<td>").text(trainFTime),
       $("<td>").text(trainFreq),
-    //   $("<td>").text(empBilled)
     );
   
     // Append the new row to the table
     $("#train-table").append(newRow);
   });
+
+
+/// need to code next arrival and minutes away outside of the .on child added function so it
+/// is continuously updated
+  var currentTime = moment().format('LT');
+  console.log(currentTime);
   
-  // Example Time Math
-  // -----------------------------------------------------------------------------
-  // Assume Employee start date of January 1, 2015
-  // Assume current date is March 1, 2016
-  
-  // We know that this is 15 months.
-  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
-  
+    newRow.append(
+    $("<tr>").append(setInterval(function() {
+    $("<td>").text(parseInt(currentTime));
+    console.log(currentTime);
+}, 1000)));
